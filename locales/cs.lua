5e0f4826b63b4dd47a09c5eb06c2093d84f3bd95
fvny <q.xn@whsrp.eu>
2026-05-31 13:29:11 +0200

    2.0 - cs

--- locales/cs.lua
+++ locales/cs.lua
@@ -5,17 +5,33 @@ Locales['cs'] = {
     ['base_salary'] = 'Základní plat: $%s',
     ['tax'] = '~n~Daň (%s%%) - $%s',
     
-    -- ox_lib specific
     ['lib_received_bank'] = '\n\nObdržel jsi: $%s',
     ['lib_received_cash'] = '\n\nObdržel jsi: $%s',
     ['lib_received_item'] = '\n\nObdržel jsi: $%s',
     ['lib_base_salary'] = 'Základní plat: $%s',
     ['lib_tax'] = '\nDaň (%s%%): -$%s',
-    ['lib_title'] = 'Výplata přijata',
+    ['lib_title'] = 'Výplata',
 
-    -- other
     ['other_received_bank'] = '\n\nUloženo v bance: $%s',
     ['other_received_cash'] = '\n\nObdrženo v hotovosti: $%s',
     ['other_received_item'] = '\n\nObdržen předmět: %s',
     ['other_tax'] = '\nDaň (%s%%): -$%s',
+
+    ['employer_broke'] = 'Tvůj zaměstnavatel neměl dostatek financí na tvoji výplatu!',
+    ['claim_paycheck'] = 'Výplata',
+    ['paycheck_claimed'] = 'Vybral jsi svou nevyzvednutou výplatu ve výši $%s',
+    ['no_pending'] = 'Nemáš žádné nevyzvednuté výplaty k vybrání.',
+    ['afk_skipped'] = 'Byl jsi AFK! Tvoje výplata byla zrušena.',
+
+    ['wallet_title'] = 'Výplata - %s',
+    ['wallet_content'] = 'Obdržel jste výplatu ve výši: $%s',
+
+    ['banker_title'] = 'Bankéř',
+    ['banker_progress'] = 'Mluvíš s bankéřem...',
+    ['banker_collected'] = 'Nadhromaděno: $%s',
+    ['banker_expires'] = '\nVyprší za: %s',
+    ['banker_no_paychecks'] = 'Žádné nevyzvednuté výplaty',
+    ['banker_no_paychecks_desc'] = 'Nemáš žádné nevyzvednuté výplaty k vybrání.',
+    ['talk_to_banker'] = '[E] Promluvit s bankéřem',
+    ['target_label'] = 'Mluvit s bankéřem'
 }
